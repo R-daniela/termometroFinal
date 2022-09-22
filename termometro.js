bc143d5077a1cbc637d01a405074bb1dfc8ff38b
Rocio Daniela Montero <rociodanielamonteroc@gmil.com>
2022-09-22 07:53:00 -0500

    termometro terminado

--- termometro.js
+++ termometro.js
@@ -1,10 +1,15 @@
 let boton = document.getElementById("boton")
+boton.addEventListener('click',resultado)
 
 function resultado(){
     const result = document.getElementById('result')
     let temperatura = document.getElementById("celcios").value;
-    let convertir =  ((temperatura - 32) * 5/9).toFixed(2)
+    let convertir =  ((temperatura - 32) * 5/9).toFixed(1)
     result.value =  convertir
+
+    document.getElementById('mercurio').style.height=convertir+'px'
+    document.getElementById('mercurio').innerText=convertir+'º'
+    document.getElementById('mercurio').style.color='white'
 }
-boton.addEventListener('click',resultado)
+
 
